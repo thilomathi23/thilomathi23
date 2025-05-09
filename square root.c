13b8e71d4807b1758b9824b0910282bacba68e97
thilomathi23 <thilomathisuresh@gmail.com>
2025-05-09 20:36:32 +0530

    square root.c

---
+++ square root.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("square root of %1f is %1f/n",225.0,sqrt(225.0));
+    printf("square root of %1f is %1f/n",300.0,sqrt(300.0));
+    
+    return 0;
+}
