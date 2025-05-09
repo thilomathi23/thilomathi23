03f2ee68226cc15ca5db3bc969f7bb9f5de75370
thilomathi23 <thilomathisuresh@gmail.com>
2025-05-09 20:43:56 +0530

    value , pow . c

---
+++ value , pow .c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("value 8.0^3 = %1f\n", pow (8.0,3));
+    printf("value 3.05^1.98 = %1f\n", pow (3.05,1.98));
+    
+    return 0;
+}
