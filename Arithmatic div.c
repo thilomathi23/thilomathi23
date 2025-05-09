85ecec10674eae6b7bee771a3988b8d96f942455
thilomathi23 <thilomathisuresh@gmail.com>
2025-05-09 20:20:53 +0530

    Arithmatic div.c

---
+++ Arithmatic div.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a, b, div;
+    
+    printf("Enter the two numbers: ");
+    scanf("%d%d", &a, &b);
+    
+    div = a / b;
+    printf("div = %d", div);
+    return 0;
+}
