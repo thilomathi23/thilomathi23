fc38728544e68e8568c696f04bd940bc13f68df9
thilomathi23 <thilomathisuresh@gmail.com>
2025-05-15 18:53:30 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+***************************************************************************/
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    printf("Enter first number : ");
+    scanf("%d", &num1);
+    printf("Enter second number : ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("sum = %d\n", sum);
+    return 0;
+}
