397aa182c2e5ba1fe1b7d7c8d0d472616af65eff
thilomathi23 <thilomathisuresh@gmail.com>
2025-05-15 18:45:00 +0530

    if.c

---
+++ if.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+***************************************************************************/
+main() 
+{
+    int a = 300, b, c ;
+    if ( a>= 400)
+         b = 300 ;
+         c = 200;
+         printf ("\n%d %d", b, c);
+}
